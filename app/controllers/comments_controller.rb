49b2646ab8e4a8006a5724be2547bbb728c13a76
Jimmy Wu <wujimmy94@gmail.com>
2017-04-28 15:30:40 -0400

    added delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,20 +11,34 @@ class CommentsController < ApplicationController
     user = User.find(session[:user_id])
 
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(params[:comment])
+    @comment = @post.comments.new(comment_params)
     puts @post
     if @comment.save
       flash[:notice] = "Comment successfully added"
       user.comments << @comment
-      redirect_to posts_path
+      redirect_to post_path(@post)
 
     else
       flash[:alert] = "Error posting comment"
-      redirect_to posts_path
+      redirect_to post_path(@post)
     end
   end
 
   def destroy
-
+    @comment = Comment.find(params[:id])
+    @post = Post.find(@comment.post_id)
+    if @comment.destroy
+      flash[:notice] = "Comment deleted"
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "There was an issue deleting comment"
+      redirect_to post_path(@post)
+    end
   end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end
